6eb487fbee818f52b754d555b56b446ce8da9a74
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-12 13:45:54 -0700

    finished step 3 in products.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useContext } from "react";
+import ProductContext from "../contexts/ProductContext";
 
-const Product = props => {
-	return (
-		<div className="product">
-			<img src={props.product.image} alt={`${props.product.title} book`} />
+const { products, addItem } = useContext(ProductContext);
 
-			<h1 className="title">{props.product.title}</h1>
+const Product = () => {
+  return ()
+    <div className="product">
+      <img src={product.image} alt={`${product.title} book`} />
 
-			<p className="price">${props.product.price}</p>
+      <h1 className="title">{product.title}</h1>
 
-			<button onClick={() => props.addItem(props.product)}>
-				Add to cart
-			</button>
-		</div>
-	);
+      <p className="price">${product.price}</p>
+
+      <button onClick={() => addItem(product)}>Add to cart</button>
+    </div>
+  );
 };
 
 export default Product;
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,1 +1,14 @@
 import React, { createContext } from "react";
+ 
+
+export default class ProductContext extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ProductContext;
